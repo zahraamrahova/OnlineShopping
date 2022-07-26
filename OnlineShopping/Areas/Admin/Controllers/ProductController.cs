a0d16642ba7b0b66ce3b1261aaf857dd1a25cdb0
zahraamrahova <zehra.khudaverdiyeva@gmail.com>
2022-07-26 14:04:45 +0300

    Product controller added

--- OnlineShopping/Areas/Admin/Controllers/ProductController.cs
+++ OnlineShopping/Areas/Admin/Controllers/ProductController.cs
@@ -20,6 +20,13 @@ namespace OnlineShopping.Areas.Admin.Controllers
         {
             return View(_db.Products.Include(c => c.ProductTypes).Include(s => s.SpecialTag).ToList());
         }
+
+        [HttpPost]
+        public IActionResult Index (decimal lowAmount, decimal largeAmount)
+        {
+            var products = _db.Products.Include(c => c.ProductTypes).Include(c => c.SpecialTag).Where(c => c.Price >= lowAmount && c.Price <= largeAmount);
+            return View(products);
+        }
         [HttpGet]
         public IActionResult Create()
         {
@@ -32,12 +39,25 @@ namespace OnlineShopping.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)
             {
+                var searchproduct = _db.Products.FirstOrDefault(c => c.Name == product.Name);
+             
+                if (searchproduct!=null)
+                {
+                    ViewBag.message = "This product is already exist";
+                    ViewData["ProductTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+                    ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "Name");
+                    return View(product);
+                }
                 if (image != null)
                 {
                     var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
                     await image.CopyToAsync(new FileStream(name, FileMode.Create));
                     product.Image = "Images/" + image.FileName;
 
+                }
+                if (image==null)
+                {
+                    product.Image = "Images/noimage.png";
                 }
                 _db.Products.Add(product);
                 await _db.SaveChangesAsync();
@@ -46,6 +66,92 @@ namespace OnlineShopping.Areas.Admin.Controllers
             }
             return View(product);
         }
+        [HttpGet]
+        public ActionResult Edit (int? id) 
+        {
+            ViewData["ProductTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+            ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "Name");
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            var product = _db.Products.Include(c=>c.ProductTypes).Include(c=>c.SpecialTag).FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                if (image != null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    product.Image = "Images/" + image.FileName;
+
+                }
+                if (image == null)
+                {
+                    product.Image = "Images/noimage.png";
+                }
+                _db.Products.Update(product);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product has been saved succefully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+
+        }
+        [HttpGet]
+        public ActionResult Details (int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Include(c=>c.ProductTypes).Include(c=>c.SpecialTag).FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+        [HttpGet]
+        public ActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Include(c => c.ProductTypes).Include(c => c.SpecialTag).FirstOrDefault(d => d.Id == id);
+            if (product==null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.FirstOrDefault(d => d.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _db.Products.Remove(product);
+           await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- OnlineShopping/Models/Product.cs
+++ OnlineShopping/Models/Product.cs
@@ -7,12 +7,12 @@ namespace OnlineShopping.Models
     {
         public int Id { get; set; }
         [Required]
-        public string? Name { get; set; }
+        public string Name { get; set; }
         [Required]
         public decimal Price { get; set; }
-        public string? Image { get; set; }
+        public string Image { get; set; }
         [Display(Name="Product Color")]
-        public string? ProductColor {get;set;}
+        public string ProductColor {get;set;}
         [Required]
         [Display(Name = "Available")]
         public bool IsAvailable { get; set; }
@@ -20,11 +20,11 @@ namespace OnlineShopping.Models
         [Required]
         public int ProductTypeId { get; set; }
         [ForeignKey("ProductTypeId")]
-        public virtual ProductTypes? ProductTypes { get; set; }
+        public virtual ProductTypes ProductTypes { get; set; }
         [Display(Name = "Special Tag")]
         [Required]
         public int SpecialTagId { get; set; }
         [ForeignKey("SpecialTagId")]
-        public virtual SpecialTag? SpecialTag { get; set; }
+        public virtual SpecialTag SpecialTag { get; set; }
     }
 }
